2370a3617b5d4f3853464770f4abe9d39f789508
Marcos728 <72949378+Marcos728@users.noreply.github.com>
2022-06-20 00:07:23 -0500

    algun cambio no se guardo en el commit anterior

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Formnulario</title>
+    <title>Formulario</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
     <script src='main.js'></script>
@@ -15,7 +15,7 @@
         <h4> Login de Acceso </h4>
         <p> usuario <input type="text" name="usuario" id="usuario" placeholder="ingrese su usuario"> </p>
         <p> contraseña <input type="text" name="contraseña" id="contraseña" placeholder="ingrese su contraseña"> </p>  
-        <input class="botons" type="submit" value="Ingresar">
+        <input class="botons botoncito" type="submit" value="Ingresar">
     </section>
     
 </body>
