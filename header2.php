38a306da8d8280229a5afd4611a8cac7aed04d1c
Eber Escobar <eber.escobar@alum.udep.edu.pe>
2022-06-20 17:46:47 -0300

    Generamos el header para las páginas

---
+++ header2.php
@@ -0,0 +1,67 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="icon" type="image/x-icon" href="images/favicon.svg">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+            
+    <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <link rel="stylesheet" href="css/general.css">
+    <title>TRABAJO SIN</title>
+</head>
+<body>
+    <?
+        
+    ?>
+
+    <header class="header">
+        <section class="flex">
+            <a href="index.php" class="logo">Tiendamanía</a>
+            <nav class="navbar">
+                <a href="#">Productos</a>
+                <a href="#">Nosotros</a>
+                <a href="#">Contacto</a>
+                <?php
+                    session_start();
+                    if(isset($_SESSION["usuario"])){
+                        $con= mysqli_connect("localhost","root","","mydb");
+                        $sql = "SELECT * FROM usuario where dni = '".$_SESSION["usuario"]."'";
+                        $result = mysqli_query($con, $sql);
+                        $user = mysqli_fetch_array($result);
+                        if($user['tipoUsuario'] == 0){
+                        }else{
+                            echo '<a href="#">Administrador</a>';
+                        }
+                    }
+                    
+                ?>
+            </nav>
+            <div class="icons">
+                <div id="menu-btn" class="fas fa-bars"></div>
+                <a href="#"><i class="fas fa-shopping-cart"></i><span></span></a>
+                <?php
+                    if(isset($_SESSION["usuario"])){
+                        $con= mysqli_connect("localhost","root","","mydb");
+                        $sql = "SELECT * FROM usuario where dni = '".$_SESSION["usuario"]."'";
+                        $result = mysqli_query($con, $sql);
+                        $user = mysqli_fetch_array($result);
+                        echo 'Hola '.$user['usuario'];
+                        
+                    }else{
+                        echo '<p> Hola usuario</p>';
+                    }
+                ?>
+                <a href="#"><i class="fas fa-user"></i><span></span></a>
+
+
+            </div>
+        </section>
+
+        <div class="profile">
+        
+            </div>
+
+        </header>
+        ';
+    
