a798003a277824d1d6fca29b6ed00ef5fb9d0d89
Lukas <lukas.arbulu@alum.udep.edu.pe>
2022-06-22 16:49:50 -0500

    agregamos el boton de agregar a carrito cuando vemos el detalle de un producto

--- show.php
+++ show.php
@@ -1,10 +1,10 @@
 <?php
     include 'header2.php';
 
-    $con= mysqli_connect("localhost","root","","mydb");
+    $con= mysqli_connect("localhost","root","","sin_grupo_2");
     $sql= "SELECT * FROM productos where idProducto = '".$_GET['id']."'";
     $result = mysqli_query($con, $sql);
-    $producto = mysqli_fetch_array($result);  
+    $producto = mysqli_fetch_array($result);
 ?>
 
 <br>
@@ -20,7 +20,13 @@
     <div class="contenedor">
         <h1><?php echo $producto['nombre']?></h1>
         <div class="caracteristicas">
-            <a href="#" class="btn">Agregar al carrito</a>
+            <?php
+                if($producto['stock' == 0]){
+                    echo '<a href="error5.php" class="btn">Agregar al carrito</a>';
+                }else{
+                    echo '<a href="#" class="btn">Agregar al carrito</a>';
+                }
+            ?>
             <div class="info">
                 <p class="precio">S/. <?php echo $producto['precio']?></p>
                 <p class="stock">Stock: <?php echo $producto['stock']?></p>
