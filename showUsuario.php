f40cd361a22c2ccbd9b82e2ddaada9dea82f1eee
Eber Escobar <eber.escobar@alum.udep.edu.pe>
2022-06-24 03:17:23 -0300

    agregamos el historial de compras en el perfil del usuario con su respectivo total

--- showUsuario.php
+++ showUsuario.php
@@ -43,7 +43,29 @@
                     <li><i class="fa-solid fa-envelope"></i> Direccion de correo: <b><?php echo $user['email']?></b></li>
                     <li><i class="fa-solid fa-person"></i> Número de identificación: <b><?php echo $user['dni']?></b></li>
                     <li><i class="fa-solid fa-people-group"></i></i> Rol: <b><?php echo $rol?></b></li>
+                    
                 </ul>
+            </div>
+
+            <div class="perfil-usuario-footer historial">
+                <p><b>Historial de compras</b></p>
+                <ul class="lista-datos">
+                    <?php
+                        $i=1;
+                        $total=0;
+                        $sql45= "SELECT * FROM boletas WHERE dniCliente='".$user['dni']."'";
+                        if($result45 = mysqli_query($con, $sql45)){
+                            while ($row = mysqli_fetch_array($result45)){
+                                echo '<li><b>'.$i.'°Compra:</b> Fecha: '.$row['fecha'].'. Consumo: S/.'.$row['total'].'</li>';
+                                $i++;
+                                $total = $total+ $row['total'];
+                            }
+                        }else{
+                            echo '<p>No ha realizado compras por el momento</p>';
+                        }    
+                    ?>
+                </ul>
+                <p><b>Total consumido: </b>S/. <?php echo $total?></p>
             </div>
         </div>
     </section>
