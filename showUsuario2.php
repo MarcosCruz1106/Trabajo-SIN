e438918d0546f31ae48d0e2dffafcd0895cd0393
Victor Alva <victor.alva@alum.udep.edu.pe>
2022-06-24 11:46:57 -0500

    ultimos cambios de la vista clientes desde adminstrador

--- showUsuario2.php
+++ showUsuario2.php
@@ -4,7 +4,16 @@
     include 'header2.php';
     if(isset($_SESSION["usuario"])){
         $con= mysqli_connect("localhost","root","","sin_grupo_2");
-        $sql = "SELECT * FROM usuario where dni = '".$_GET['id']."'";
+        if(isset($_GET['id'])){
+            $sql = "SELECT * FROM usuario where dni = '".$_GET['id']."'";
+            $sql45= "SELECT * FROM boletas WHERE dniCliente='".$_GET['id']."'";
+
+        }else{
+            $sql = "SELECT * FROM usuario where dni = '0'";
+            $sql45= "SELECT * FROM boletas WHERE dniCliente='0'";
+
+
+        }
         $result = mysqli_query($con, $sql);
         $user = mysqli_fetch_array($result);
     }else{
@@ -15,10 +24,12 @@
             </div>';
     }
 
-    if($user['tipoUsuario']==0){
-        $rol='cliente';
-    }else{
-        $rol='administrador';
+    if(isset($user)){
+        if($user['tipoUsuario']==0){
+            $rol='cliente';
+        }else{
+            $rol='administrador';
+        }
     }
 ?>
 
@@ -35,14 +46,15 @@
         </div>
         <div class="perfil-usuario-body">
             <div class="perfil-usuario-bio">
-                <h3 class="titulo"><?php echo $user['nombres'].' '.$user['apellidos']; ?></h3>
-                <p class="texto">El usuario <?php echo $user['nombres'].' '.$user['apellidos']; ?> con nickname <b><?php echo $user['usuario']?></b> tiene el rol de <?php echo $rol?>.</p>
+
+                <h3 class="titulo"><?php if(isset($user)){ echo $user['nombres'].' '.$user['apellidos'];} ?></h3>
+                <p class="texto">El usuario <?php  if(isset($user)){ echo $user['nombres'].' '.$user['apellidos'];} ?> con nickname <b><?php  if(isset($user)){echo $user['usuario'];}?></b> tiene el rol de <?php  if(isset($rol)){echo $rol;}?>.</p>
             </div>
             <div class="perfil-usuario-footer">
                 <ul class="lista-datos">
-                    <li><i class="fa-solid fa-envelope"></i> Direccion de correo: <b><?php echo $user['email']?></b></li>
-                    <li><i class="fa-solid fa-person"></i> Número de identificación: <b><?php echo $user['dni']?></b></li>
-                    <li><i class="fa-solid fa-people-group"></i></i> Rol: <b><?php echo $rol?></b></li>
+                    <li><i class="fa-solid fa-envelope"></i> Direccion de correo: <b><?php  if(isset($user)){echo $user['email'];}?></b></li>
+                    <li><i class="fa-solid fa-person"></i> Número de identificación: <b><?php  if(isset($user)){echo $user['dni'];}?></b></li>
+                    <li><i class="fa-solid fa-people-group"></i></i> Rol: <b><?php  if(isset($rol)){ echo $rol;}?></b></li>
                     
                 </ul>
             </div>
@@ -53,12 +65,19 @@
                     <?php
                         $i=1;
                         $total=0;
-                        $sql45= "SELECT * FROM boletas WHERE dniCliente='".$_GET['id']."'";
+                        if(isset($_GET['id'])){
+                            $sql45= "SELECT * FROM boletas WHERE dniCliente='".$_GET['id']."'";
+                        }else{
+                            $sql45= "SELECT * FROM boletas WHERE dniCliente='0'";
+
+                        }
                         if($result45 = mysqli_query($con, $sql45)){
-                            while ($row = mysqli_fetch_array($result45)){
-                                echo '<li><b>'.$i.'°Compra:</b> Fecha: '.$row['fecha'].'. Consumo: S/.'.$row['total'].'</li>';
-                                $i++;
-                                $total = $total+ $row['total'];
+                            if(isset($result45)){
+                                while ($row = mysqli_fetch_array($result45)){
+                                    echo '<li><b>'.$i.'°Compra:</b> Fecha: '.$row['fecha'].'. Consumo: S/.'.$row['total'].'</li>';
+                                    $i++;
+                                    $total = $total+ $row['total'];
+                                }
                             }
                         }
                     ?>
